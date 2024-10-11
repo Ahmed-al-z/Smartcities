22f70340eb36bd01b1a2d7f9c1400c59da142dad
Ahmed-al-z <ahmed.al-zubaidi@student.hepl.be>
2024-10-11 17:13:11 +0200

    lesson12-proj2

--- LED_neo/lesson12-proj2.py
+++ LED_neo/lesson12-proj2.py
@@ -10,5 +10,18 @@ ss=ADC(1)
 while True:
     light= ls.read_u16()/256
     noise = ss.read_u16()/256
-    print(light)
-    
+    print(light,noise)
+
+    if light < 80:
+        led.pixels_fill((255,255,255))
+        led.pixels_show()
+        sleep(0.1)
+    else:
+        if noise > 50:
+            led.pixels_fill((255,0,0))
+            led.pixels_show()
+            sleep(1)
+        else:
+            led.pixels_fill((0,255,0))
+            led.pixels_show()
+            sleep(1)
