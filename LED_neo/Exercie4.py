c2a58e303aa28b50c49e90ce593660eaa3d26ea4
Ahmed-al-z <ahmed.al-zubaidi@student.hepl.be>
2024-10-13 00:21:12 +0200

    1.01

--- LED_neo/Exercie4.py
+++ LED_neo/Exercie4.py
@@ -4,7 +4,7 @@
 -changer la couleur de la LED aleatoirement a chaque battement.         
 
 '''
-import machine 
+import machine
 from ws2812 import WS2812
 from machine import Pin,ADC
 from time import sleep
@@ -23,19 +23,25 @@ purple = (180,0,255)
 white = (255,255,255)
 
 colors=(black,red,yellow,green,cyan,blue,purple,white)
+rancol = random.choice(colors)
 
-'''on va mettre une condition qui dit: si le son depasse on change de couleur
+'''
+on va mettre une condition qui dit: si le son depasse on change de couleur
 pour ca on aura une variable pour le seuill une pour le debounce et une qui servira
 a mettre a jour le temps.
 et on va utiliser la fonction utime_ticks_ms qui sert a compter le temps depuis
-la mise sous tension du RPI
+la mise sous tension du RPI.
 '''
 
-seuil= 500 # seuille a partir du quelle on detecte un pic de son
-
+seuil= 0 # seuille a partir du quelle on detecte un pic de son
+previous = 0
 debounce = 300
 
 
 while True:
     audio = ss.read_u16()/256 #for the rgb range 
 
+    if audio > seuil:
+        led.pixels_fill(rancol)
+        led.pixels_show()
+        sleep(0.2)
